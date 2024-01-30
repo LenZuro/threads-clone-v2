daf651c2da6d7f2f4c3a27effa812d24373c133a
LenZuro <126126939+LenZuro@users.noreply.github.com>
2024-01-29 16:46:44 -0800

    User action created

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,34 @@
 "use server"
 
-import { connectToDb } from "../mongoose"
+import { connectToDb } from "../mongoose";
+import User from "../models/user.model";
+import { revalidatePath } from "next/cache";
 
 
-export async function updateUSer(): Promise<void> {
+export async function updateUSer(
+    userID: string ,
+    username: string,
+    name: string,
+    bio: string,
+    image: string,
+    path: string,
+    ): Promise<void> {
     connectToDb();
-
-    
+        try {
+            await User.findOneAndUpdate(
+                { id: userID },
+                { username: username.toLowerCase(),
+                    name ,
+                    bio ,
+                    image ,
+                    onboarded: true ,
+                },
+                { upsert: true }
+            );
+            if(path === '/profile/edit'){
+                revalidatePath(path);
+            }
+        } catch (error: any) {
+            throw new Error(`Failed to create/update user : ${error.message}`)
+        }
 }
