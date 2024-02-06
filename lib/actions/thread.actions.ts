bab23954069fc90d790b65ad6526645a218b7447
LenZuro <126126939+LenZuro@users.noreply.github.com>
2024-02-05 17:19:28 -0800

    Functionailty should be working. Punycode issue

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -5,6 +5,19 @@ import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { revalidatePath } from "next/cache";
 
+export async function fetchThreads(pageNumber = 1 , pageSize = 20) {
+    connectToDb();
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const threadQuery = Thread.find({ parentId: { $in: [null , undefined ]}})
+    .sort({ createdAt: 'desc' }).skip(skipAmount).limit(pageSize)
+    .populate({ path: 'author' , model: User })
+    .populate({ path: 'children' , populate: { path: 'author' , model: User , select: "_id name parentId image" }})
+    const totalThreadsCount = await Thread.countDocuments({ parentId: { $in: [ null , undefined ]}})
+    const threads = await threadQuery.exec();
+    const isNext = totalThreadsCount > skipAmount + threads.length;
+    return { threads , isNext}
+}
+
 interface Params {
     text: string,
     author: string,
@@ -23,19 +36,6 @@ export async function createThread({ text , author , communityId , path }: Param
     } 
 }
 
-export async function fetchThreads(pageNumber = 1 , pageSize = 20) {
-    connectToDb();
-    const skipAmount = (pageNumber - 1) * pageSize;
-    const threadQuery = Thread.find({ parentId: { $in: [null , undefined ]}})
-    .sort({ createdAt: 'desc' }).skip(skipAmount).limit(pageSize)
-    .populate({ path: 'author' , model: User })
-    .populate({ path: 'children' , populate: { path: 'author' , model: User , select: "_id name parentId image" }})
-    const totalThreadsCount = await Thread.countDocuments({ parentId: { $in: [ null , undefined ]}})
-    const threads = await threadQuery.exec();
-    const isNext = totalThreadsCount > skipAmount + threads.length;
-    return { threads , isNext}
-}
-
 export async function fetchThreadById( id: string ) {
     connectToDb();
     try {
@@ -48,5 +48,22 @@ export async function fetchThreadById( id: string ) {
         return thread;
     } catch (error : any) {
         throw new Error(`Error fetching thread: ${error.message}`)
+    }
+}
+
+export async function addCommentToThread( threadId: string , commentText: string , userId: string , path: string) {
+    connectToDb();
+    try {
+        const originalThread = await Thread.findById(threadId);
+        if (!originalThread) {
+            throw new Error('Thread not found');
+        }
+        const commentThread = new Thread({ text: commentText , author: userId , parentId: threadId});
+        const savedCommentThread = await commentThread.save();
+        originalThread.children.push(savedCommentThread._id);
+        await originalThread.save();
+        revalidatePath(path);
+    } catch (error : any) {
+        throw new Error(`Unable to add comment ${error.message}`)
     }
 }
