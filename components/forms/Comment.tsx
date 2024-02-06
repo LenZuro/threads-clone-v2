0092b16d10c48a0ac3e394c43e2b856a8f382882
LenZuro <126126939+LenZuro@users.noreply.github.com>
2024-02-05 16:26:23 -0800

    Added comment form beofre adding comment functions

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -8,19 +8,57 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Form , FormControl, FormField, FormItem, FormLabel } from '../ui/form';
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
+import { CommentValidation } from '@/lib/validations/thread';
+// TODO :  Add comment Functionality
 
 interface Props {
     threadId: string;
     currentUserImg: string;
     currentUserId: string;
 }
-
 const Comment = ({ threadId, currentUserImg , currentUserId } : Props) => {
+    const pathname = usePathname();
+    const form = useForm<z.infer<typeof CommentValidation>>({
+        resolver: zodResolver(CommentValidation),
+        defaultValues: { thread: "",},});
+    const onSubmit = async (values: z.infer<typeof CommentValidation>) =>{
+        // Add Comment Functionality goes here
+    }
     return (
-        <div>
-            <h1 className="text-white">Comment Form</h1>
-        </div>
-    )
+        <Form {...form}>
+        <form className='comment-form' onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name='thread'
+            render={({ field }) => (
+              <FormItem className='flex w-full items-center gap-3'>
+                <FormLabel>
+                  <Image
+                    src={currentUserImg}
+                    alt='current_user'
+                    width={48}
+                    height={48}
+                    className='rounded-full object-cover'
+                  />
+                </FormLabel>
+                <FormControl className='border-none bg-transparent'>
+                  <Input
+                    type='text'
+                    {...field}
+                    placeholder='Comment...'
+                    className='no-focus text-light-1 outline-none'
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+  
+          <Button type='submit' className='comment-form_btn'>
+            Reply
+          </Button>
+        </form>
+      </Form>
+    );
 }
 
 export default Comment;
